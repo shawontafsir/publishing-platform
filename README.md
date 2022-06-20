b386e3f4b581e1e5c9d7ac39ac569f5721da76ba
Khalid Hasan <khalid@intelligentmachin.es>
2022-06-20 12:24:06 +0600

    Readme is updated

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# publishing-platform
+# Simple Content Publishing Platform
+
+## To Run The project:
+
+### Back-end:
+```
+// go to the backend directory
+$ cd backend
+
+// create a python virtualenv
+$ virtualenv venv
+
+// activate the venv
+$ source venv/bin/activate
+
+// install dependencies
+$ pip install -r requirements.txt
+
+// run the project at port 8000
+$ ./manage.py runserver 8000
+```
+Now, the **Backend** is ready to serve the **Rest API**.
+
+
+### Front-end:
+```
+// go to the frontend directory
+$ cd frontend
+
+// install node dependencies locally
+$ npm install
+
+// run the parcel bundler
+$ parcel index.html
+```
+Now, the **Frontend** is ready. Open the url built by the `parcel bundler` to the **browser**.
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders.apps.CorsHeadersAppConfig',
+    'rest_framework.apps.RestFrameworkConfig',
     'users.apps.UsersConfig',
     'contents.apps.ContentsConfig'
 ]
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,6 +128,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 AUTH_USER_MODEL = 'users.User'
 
